30149331306b37f2963d3f972dcfd7e7f2f599ae
Kotejichizu <n.golawski@wp.pl>
2024-12-03 16:29:14 +0100

    Reusable input reading tool

---
+++ InputReader/InputReader.java
@@ -0,0 +1,21 @@
+package InputReader;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class InputReader {
+    private ArrayList<String> input = new ArrayList<>();
+
+    public ArrayList<String> getInput(String file) {
+        try (Scanner fileScanner = new Scanner(Paths.get(file))) {
+            while (fileScanner.hasNextLine()) {
+                String row = fileScanner.nextLine();
+                input.add(row);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return input;
+    }
+}
